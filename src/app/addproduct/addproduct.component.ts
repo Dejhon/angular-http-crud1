c6bcd08cfc2dbc97c2932d1c204717e379d981cf
Dejhon <dejhonsherman6@gmail.com>
2022-06-23 18:37:38 -0500

    updates

--- src/app/addproduct/addproduct.component.ts
+++ src/app/addproduct/addproduct.component.ts
@@ -3,6 +3,7 @@ import { Validators, FormControl, FormGroup } from '@angular/forms';
 import { DataService } from '../data.service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-addproduct',
   templateUrl: './addproduct.component.html',
@@ -14,12 +15,15 @@ export class AddproductComponent implements OnInit {
 
   submitted = false
 
+  idNumber:any = Math.floor(Math.random() *1000);
+
   Items = new FormGroup({
-    itemName: new FormControl('',Validators.required),
-    itemDescription: new FormControl('', Validators.required),
-    itemPrice: new FormControl('', Validators.required),
-    itemUrl : new FormControl('', Validators.required),
-    itemQuantity: new FormControl('', Validators.required)
+    id:  new FormControl(this.idNumber),
+    name: new FormControl('',Validators.required),
+    description: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    imageUrl : new FormControl('', Validators.required),
+    quantity: new FormControl('', Validators.required)
   })
 
   get itemName(){
@@ -42,37 +46,51 @@ export class AddproductComponent implements OnInit {
     return this.Items.get('itemQuantity')
   }
 
-
   onSubmit(){
-    this.submitted = true;
+    this.submitted = true
     if(this.Items.valid && this.submitted == true){
-  
-      const data ={
-        name: this.itemName?.value,
-        description: this.itemDescription?.value,
-        price: this.itemPrice?.value,
-        url: this.itemUrl?.value,
-        quantity: this.itemQuantity?.value
-      }
-  
-      const itemInfo = new FormData()
-        itemInfo.append('name',data.name);
-        itemInfo.append('description',data.description);
-        itemInfo.append('price',data.price);
-        itemInfo.append('url',data.url);
-        itemInfo.append('quantity',data.quantity);
-  
-          this.dataservice.additem(itemInfo).subscribe({
-            next:()=>{
-              alert('Sign up successful');
-              this.route.navigate(['/'])
-            },
-            error:()=>{
-              alert('Error While Adding product')
-            }
-          })
+      this.dataservice.additem(this.Items.value).subscribe({
+        next:()=>{
+          alert('Item Added to Database')
+          this.route.navigate(['/'])
+        },
+        error:()=>{
+          alert('Error')
         }
+      })
+    }
   }
+
+  // onSubmit(){
+  //   this.submitted = true;
+  //   if(this.Items.valid && this.submitted == true){
+  
+  //     const data ={
+  //       name: this.itemName?.value,
+  //       description: this.itemDescription?.value,
+  //       price: this.itemPrice?.value,
+  //       url: this.itemUrl?.value,
+  //       quantity: this.itemQuantity?.value
+  //     }
+  
+  //     const itemInfo = new FormData()
+  //       itemInfo.append('name',data.name);
+  //       itemInfo.append('description',data.description);
+  //       itemInfo.append('price',data.price);
+  //       itemInfo.append('url',data.url);
+  //       itemInfo.append('quantity',data.quantity);
+  
+  //         this.dataservice.additem(itemInfo).subscribe({
+  //           next:()=>{
+  //             alert('Sign up successful');
+  //             this.route.navigate(['/'])
+  //           },
+  //           error:()=>{
+  //             alert('Error While Adding product')
+  //           }
+  //         })
+  //       }
+  // }
 
   ngOnInit(): void {
   }
